43e2d5aac9b5e93f5d7e23048cb2d534f451d177
Jedediah Arnold <jarnold910@gmail.com>
2020-09-14 21:38:50 -0400

    add code for the database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -10,10 +11,16 @@ APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to applicati
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///no-db-yet')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY', 'secret_backup')
 # Redirects are not blocked here - set this next line to True or delet it in order to enable them
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+connect_db(app)
 
 # HOME PAGE ROUTE
 @app.route('/')
