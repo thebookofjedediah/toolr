917157d4320d5cc1ef5f2d6c8f76fdc872805e8c
Jedediah Arnold <jarnold910@gmail.com>
2020-09-24 17:32:36 -0400

    starting over with socketio

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from models import db, connect_db, User, Tool
 from dotenv import load_dotenv
 from forms import UserRegistrationForm, UserLoginForm, ToolAddForm
 from sqlalchemy.exc import IntegrityError
-from flask_socketio import SocketIO, join_room, leave_room
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
@@ -197,29 +197,3 @@ def get_tool_information(username, toolID):
 # ******************
 
 # ENTER CHAT ROOM
-@app.route('/users/<username>/tools/<toolID>/chat')
-def get_chat_room(username, toolID):
-    if "username" not in session:
-        flash("You are not authorized to view that page", "danger")
-        return redirect('/')
-
-    curr_username = session["username"]
-    user = User.query.filter_by(username=curr_username).first()
-    room = username + toolID + curr_username
-    print("Room**************Room")
-    print(room)
-    print("Room**************Room")
-    return render_template('chat.html', user=user, room=room)
-
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
