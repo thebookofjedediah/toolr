b93c7c7505aca24d3beece1d443828b0c29c21b6
Jedediah Arnold <jarnold910@gmail.com>
2020-10-05 12:36:18 -0400

    map and list view working

--- static/scripts.js
+++ static/scripts.js
@@ -1,2 +1,14 @@
+$listButton = $('#show-list');
+$mapButton = $('#show-map');
+$listView = $('#list-view')
+$mapView = $('#map-view')
 
+$listButton.on('click', function() {
+    $listView.show();
+    $mapView.hide();
+})
 
+$mapButton.on('click', function() {
+    $mapView.show();
+    $listView.hide();
+})
